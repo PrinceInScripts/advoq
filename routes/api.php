03fe6fd98934d6af0371997ec33b423e1fdec417
Jaykishan Rawat <jksonu1436@gmail.com>
2025-03-12 19:06:24 +0530

    GG

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/upload', [App\Http\Controllers\UploadController::class, 'store'])->name('uploadFiles');
+Route::post('/uploadVideo', [App\Http\Controllers\UploadController::class, 'storeVideo'])->name('uploadVideo');
