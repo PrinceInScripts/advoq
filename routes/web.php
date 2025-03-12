46c5669f46f69c591a80370e49df24e1136a32f9
Jaykishan Rawat <jksonu1436@gmail.com>
2025-03-12 19:33:00 +0530

    HH

--- routes/web.php
+++ routes/web.php
@@ -47,16 +47,22 @@ Route::post('/contact/submit', function (Request $request) {
 
 
 // Admin Routes
-Route::get('/admin/add-images', [AdminController::class, 'addIIA'])->name('admin.add-images');
-Route::get('/admin/add-video', [AdminController::class, 'addVideo'])->name('admin.add-video');
-Route::get('/get-all-images', [AdminController::class, 'getAllImages'])->name('getAllImages');
-Route::delete('/delete-image/{id}', [AdminController::class, 'destroyImages'])->name('deleteImage');
-Route::delete('/delete-video/{id}', [AdminController::class, 'destroyVideos'])->name('deleteVideo');
-Route::get('/get-all-videos', [AdminController::class, 'getAllVideos'])->name('getAllVideos');
-Route::get('/admin', function () {
-    $contacts=DB::table('contacts')->get();
-    return view('admin.dashboard', compact('contacts'));
-})->name('admin.dashboard');
+// Grouping admin routes under auth middleware
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin/add-images', [AdminController::class, 'addIIA'])->name('admin.add-images');
+    Route::get('/admin/add-video', [AdminController::class, 'addVideo'])->name('admin.add-video');
+    Route::get('/get-all-images', [AdminController::class, 'getAllImages'])->name('getAllImages');
+    Route::delete('/delete-image/{id}', [AdminController::class, 'destroyImages'])->name('deleteImage');
+    Route::delete('/delete-video/{id}', [AdminController::class, 'destroyVideos'])->name('deleteVideo');
+    Route::get('/get-all-videos', [AdminController::class, 'getAllVideos'])->name('getAllVideos');
+
+    Route::get('/admin', function () {
+        $contacts = DB::table('contacts')->get();
+        return view('admin.dashboard', compact('contacts'));
+    })->name('admin.dashboard');
+});
+
+
 Route::get('/admin/login', function () {
     return view('admin.login');
 })->name('admin.login');
